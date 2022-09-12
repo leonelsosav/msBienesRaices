60c43252123e2c76f2f8b77c022083a0a4488d6d
leonelsosav <ernesto.leonel.vera.sosa@gmail.com>
2022-09-12 02:03:37 -0500

    ficha tecnica styling

--- src/pages/FichaTecnica.jsx
+++ src/pages/FichaTecnica.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import DAO from "../components/logic/DAO";
 import "../components/style/sheetStyle.css";
+import { TbBath, TbBed, TbCar, TbToiletPaper } from "react-icons/tb";
+import { BsHouseDoor, BsBricks, BsCalendarDate } from "react-icons/bs";
 
 const FichaTecnica = () => {
   const [propiedadId, setPropiedadId] = useState(useParams().id);
@@ -21,40 +23,76 @@ const FichaTecnica = () => {
     <>
       {propiedad.Titulo && (
         <div className="sheet">
-          <img
-            src={propiedad.Imagenes[0]}
-            className="portada-ficha"
-            alt="imagen de portada"
-          />
-          <p className="titulo-ficha">{propiedad.Titulo}</p>
-          <p className="feature-ficha">{propiedad.Cuartos}</p>
-          <p className="feature-ficha">{propiedad.Banos}</p>
-          <p className="feature-ficha">{propiedad.MediosBanos}</p>
-          <p className="feature-ficha">{propiedad.Estacionamientos}</p>
-          <p className="feature-ficha">{propiedad.Construccion}</p>
-          <p className="feature-ficha">{propiedad.Terreno}</p>
-          <p className="feature-ficha">{propiedad.Antiguedad}</p>
-          <p className="titulo-ficha">{propiedad.Titulo}</p>
-          <p className="titulo-ficha">{propiedad.Titulo}</p>
-          <p className="precio-ficha">{propiedad.Precio}</p>
-          <p className="descripcion-ficha">{propiedad.Descripcion}</p>
-          {propiedad.Caracteristicas.map((v, idx) => {
-            return (
-              <p key={idx} className="caracteristicas-ficha">
-                {v}
-              </p>
-            );
-          })}
-          {propiedad.Imagenes.slice(1).map((v, idx) => {
-            return (
+          <div className="sheet-workspace">
+            <div className="portada-container">
               <img
-                key={idx}
-                src={v}
-                className="imagen-ficha"
-                alt="imagen de propiedad"
+                src={propiedad.Imagenes[0]}
+                className="portada-ficha"
+                alt="imagen de portada"
               />
-            );
-          })}
+              <div className="portada-banner">
+                <p className="titulo-ficha">{propiedad.Titulo}</p>
+                <div className="features-banner">
+                  <TbBed className="icon-ficha" />
+                  <TbBath className="icon-ficha" />
+                  <TbToiletPaper className="icon-ficha" />
+                  <TbCar className="icon-ficha" />
+                  <BsBricks className="icon-ficha" />
+                  <BsHouseDoor className="icon-ficha" />
+                  <BsCalendarDate className="icon-ficha" />
+                  <p className="green feature-ficha">{propiedad.Cuartos}</p>
+                  <p className="green feature-ficha">{propiedad.Banos}</p>
+                  <p className="green feature-ficha">{propiedad.MediosBanos}</p>
+                  <p className="green feature-ficha">
+                    {propiedad.Estacionamientos}
+                  </p>
+                  <p className="green feature-ficha">
+                    {propiedad.Construccion}
+                  </p>
+                  <p className="green feature-ficha">{propiedad.Terreno}</p>
+                  <p className="green feature-ficha">
+                    {propiedad.Antiguedad + (propiedad.Antiguedad === 1
+                      ? " año"
+                      : " años")}
+                  </p>
+                  <p className="feature-ficha">Recamaras</p>
+                  <p className="feature-ficha">Baños</p>
+                  <p className="feature-ficha">Medios baños</p>
+                  <p className="feature-ficha">Estacionamientos</p>
+                  <p className="feature-ficha">M² Construccion</p>
+                  <p className="feature-ficha">M² Terreno</p>
+                  <p className="feature-ficha">Antiguedad</p>
+                </div>
+              </div>
+            </div>
+            <div className="info-container-ficha">
+              <div className="info-ficha-izquierda">
+                <p className="titulo-ficha">{propiedad.Titulo}</p>
+                <p className="precio-ficha">Precio: ${propiedad.Precio}</p>
+                <p className="descripcion-ficha">{propiedad.Descripcion}</p>
+                {propiedad.Caracteristicas.map((v, idx) => {
+                  return (
+                    <p key={idx} className="caracteristicas-ficha">
+                      -{v}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <p className="titulo-ficha">Fotos:</p>
+            <div className="photos-container">
+              {propiedad.Imagenes.slice(1).map((v, idx) => {
+                return (
+                  <img
+                    key={idx}
+                    src={v}
+                    className="imagen-ficha"
+                    alt="imagen de propiedad"
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
       )}
     </>
